ed642341c8404be9f1a7a18ffb76dff5e284f61f
sddeveloper1 <120973377+MuhammadHamzaMahboob@users.noreply.github.com>
2023-02-10 01:44:08 +0500

    Added loader (#2)
    * loader added
    
    * enhancement
    
    * comments removed

--- screens/Result.js
+++ screens/Result.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 import { Card } from 'react-native-elements';
+import Ionicons from "react-native-vector-icons/Ionicons";
 import styles from './Style';
 
 export default class Result extends React.Component {
@@ -8,21 +9,15 @@ export default class Result extends React.Component {
     return (
       <View style={styles.containersi}>
         <View style={styles.button}>
-          <Button
-            title="<<----"
-            onPress={() => this.props.navigation.goBack()} />
+            <Ionicons name="ios-arrow-back" size={25} color="#000" onPress={() => this.props.navigation.goBack()}/>
         </View>
-        <View>
           <View style={{ marginTop: 150 }}>
             <Card >
               <Text style={styles.paragraph}>
                 This is a card from the react-native-elements
               </Text>
             </Card>
-
           </View>
-
-        </View>
       </View>
     );
   }
